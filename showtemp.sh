57137c62c7f41e84c180263260e41515c93ad0bd
chuacw <1757930+chuacw@users.noreply.github.com>
2017-10-21 14:00:30 +0800

    Aligned code
    Aligned code

--- showtemp.sh
+++ showtemp.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # chuacw 21 Oct 2017 for Linksys WRT1900AC
-echo
-echo "Shows the current temperature for the CPU, memory and WIFI sensors"
-echo "by chuacw 21 Oct 2017"
-echo
+
+  echo
+  echo "Shows the current temperature for the CPU, memory and WIFI sensors"
+  echo "by chuacw 21 Oct 2017"
+  echo
+
   cpu=`cat /sys/class/hwmon/hwmon2/temp1_input`
   ddr=`cat /sys/class/hwmon/hwmon1/temp1_input`
   wifi=`cat /sys/class/hwmon/hwmon1/temp2_input`
