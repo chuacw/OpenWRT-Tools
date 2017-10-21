f9a9e710a5de1d2350f8eb8a880a7eb3ff9929aa
chuacw <1757930+chuacw@users.noreply.github.com>
2017-10-21 13:57:18 +0800

    Scripts for Linksys WRT1900AC router
    Scripts to monitor fan status on Linksys WRT1900AC and block known ad
    hosts

---
+++ showtemp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# chuacw 21 Oct 2017 for Linksys WRT1900AC
+echo
+echo "Shows the current temperature for the CPU, memory and WIFI sensors"
+echo "by chuacw 21 Oct 2017"
+echo
+  cpu=`cat /sys/class/hwmon/hwmon2/temp1_input`
+  ddr=`cat /sys/class/hwmon/hwmon1/temp1_input`
+  wifi=`cat /sys/class/hwmon/hwmon1/temp2_input`
+  fan=`cat /sys/devices/platform/pwm_fan/hwmon/hwmon0/pwm1`
+
+# Divide by 1000 to get temp values in C
+  cpu=`expr $cpu / 1000`
+  ddr=`expr $ddr / 1000`
+  wifi=`expr $wifi / 1000`
+  echo "CPU: $cpu, DDR: $ddr, WIFI: $wifi, fan: $fan"
+  echo
+
